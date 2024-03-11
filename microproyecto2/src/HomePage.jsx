cbc96021beecc4bb86d77763f8fb2b1e8f6fce0d
andreapintoc <andrea.pinto@correo.unimet.edu.ve>
2024-03-11 17:54:35 -0400

    Pagina de club y videojuegos lista

--- microproyecto2/src/HomePage.jsx
+++ microproyecto2/src/HomePage.jsx
@@ -1,21 +1,71 @@
-import React, { useState } from "react";
-import app from "./firebase";
-import { getAuth,signOut } from "firebase/auth";
-const auth = getAuth(app)
+import React, { useState } from 'react';
+import './HomePage.css';
+import clubsData from './clubs.json';
+import videojuegosData from './videojuegos.json';
 
+function ClubList() {
+  const [selectedClub, setSelectedClub] = useState(null);
 
+  const handleClubClick = (club) => {
+    setSelectedClub(club);
+  };
 
-const HomePage = () => {
-    return(
-        <div className="xx">HOMEPAGE
-            <button className="logout" onClick={()=>signOut(auth)}>Salir</button>
-        </div>
+  const handleBackClick = () => {
+    setSelectedClub(null);
+  };
 
-       
-    )
-    }
-   
+  const getVideojuegoById = (id) => {
+    return videojuegosData.find((videojuego) => videojuego.ID === id);
+  };
 
+  const handleAfiliarseClick = () => {
+    //se coloca codigo para afiliarse al club
+  };
 
+  if (selectedClub) {
+    return (
+      <div className="club-info-container">
+        <h2 className="club-info-title">{selectedClub.nombre}</h2>
+        <p className="club-info-description">{selectedClub.descripcion}</p>
+        <h3 className="videojuegos-title">Videojuegos:</h3>
+        <ul className="videojuegos-list">
+          {selectedClub.videojuegos.map((videojuegoId) => {
+            const videojuego = getVideojuegoById(videojuegoId);
+            return (
+              <li key={videojuego.ID} className="videojuego-item">
+                <h4>{videojuego.titulo}</h4>
+                <p>Género: {videojuego.genero}</p>
+                <p>{videojuego.descripcion}</p>
+              </li>
+            );
+          })}
+        </ul>
+        <button className="afiliarse-button" onClick={handleAfiliarseClick}>
+          Afiliarse a este club
+        </button>
+        <button className="back-button" onClick={handleBackClick}>
+          Volver al listado
+        </button>
+      </div>
+    );
+  }
 
-export default HomePage;
+  return (
+    <div className="club-list-container">
+      <h1 className="club-list-title">Bienvenido al Club de Videojuegos!</h1>
+      <ul className="club-list">
+        {clubsData.map((club) => (
+          <li key={club.ID} className="club-item">
+            <h2 className="club-name">{club.nombre}</h2>
+            <p className="club-description">{club.descripcion}</p>
+            <button className="club-button" onClick={() => handleClubClick(club)}>
+              Ver más
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ClubList;
